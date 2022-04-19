5babd4981c8607daccdbd7ec4890516003f81330
Rakesh8007 <100844268+Rakesh8007@users.noreply.github.com>
2022-04-19 17:52:22 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,7 @@
 
     
   
-    scp -i root/pun-off-mumbai-kp.pem -r /var/lib/jenkins/workspace/ngautomation/dist/automationdemo ec2-user@13.235.245.21:/home/ec2-user/Jenkins-Ang-Demo1
+    scp -i root/pun-off-mumbai-kp.pem -r /var/lib/jenkins/workspace/ngautomation/dist/automationdemo/ ec2-user@13.235.245.21:/var/www/html
     
      
     exit
