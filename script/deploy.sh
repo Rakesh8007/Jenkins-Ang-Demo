411bca2ad8e174ac1f7680a5f1a683f210b3a191
Rakesh8007 <100844268+Rakesh8007@users.noreply.github.com>
2022-04-19 17:05:20 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,7 @@ ssh -tt ec2-user@13.235.245.21<<EOF
     cd ~/Jenkins-Ang-Demo
     git pull origin main
     $ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-    . ~/.nvm/nvm.sh
+    source ~/.nvm/nvm.sh
     nvm install node
     nvm install v10.11.0
     npm install
