a1062a2f450911e9cff195ce6dbd5f159828e056
Rakesh8007 <100844268+Rakesh8007@users.noreply.github.com>
2022-04-19 17:01:55 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,8 +3,9 @@
 ssh -tt ec2-user@13.235.245.21<<EOF
     cd ~/Jenkins-Ang-Demo
     git pull origin main
-    $ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-    source ~/.nvm/nvm.sh
+    $ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+    . ~/.nvm/nvm.sh
+    nvm install node
     nvm install v10.11.0
     npm install
     npm install pm2 -g
