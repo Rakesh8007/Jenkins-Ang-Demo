986ec4da7cde9656ac0c324a1be0e83e3fa084e5
Rakesh8007 <100844268+Rakesh8007@users.noreply.github.com>
2022-04-19 17:34:31 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,7 +2,7 @@
 
 
     
-    scp -i /var/lib/jenkins/workspace/ngautomation/dist/automationdemo ~/Jenkins-Ang-Demo1 
+    scp -i /var/lib/jenkins/workspace/ngautomation/dist/automationdemo  ec2-user@13.235.245.21:/home/ec2-user/Jenkins-Ang-Demo1
     
      
     exit
