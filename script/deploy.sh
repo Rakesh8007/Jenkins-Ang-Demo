4a56b280b1369849a2354ea07a393dccf21f20ed
Rakesh8007 <100844268+Rakesh8007@users.noreply.github.com>
2022-04-20 20:00:30 +0530

    Update deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-scp -i root/pun-off-mumbai-kp.pem -r /var/lib/jenkins/workspace/ngautomation/dist/automationdemo/ ec2-user@13.235.245.21:/var/www/html
+scp -i root/pun-off-mumbai-kp.pem -r /var/lib/jenkins/workspace/ngautomation/dist/automationdemo/ ec2-user@65.0.96.186:/var/www/html
 sudo systemctl restart httpd
 #ssh -1 /path/keypair.pem username@public_dns_name
 #ssh -i /var/lib/jenkins/workspace/ngautomation/dist/automationdemo/pun-off-mumbai-kp.pem ec2-user@13.235.245.21
